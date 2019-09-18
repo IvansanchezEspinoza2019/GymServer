1f5e754ab59b13af10c9d48fdd22ec3e19497cba
Ivan Sanchez <edgarse1945@gmail.com>
2019-09-18 17:59:07 -0500

    Funciones agregar y mostrar clientes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         echo json_encode(array('status'=> false));
         exit;
     }
+
 $postdata = file_get_contents("php://input");
 $datos = json_decode($postdata, true);
 
@@ -15,13 +16,182 @@ switch($datos['funcion']){
     case 'login':
         login($datos);
         break;
+    case 'getLastId':
+        getLastId($datos);
+        break;
+    case 'addCliente':
+        addCliente($datos);
+        break;
+    case 'getAllCustomers':
+        getAllCustomers();
+        break;
     default:
         echo json_encode("-1");
 
 
 }
+function getAllCustomers(){
+    include "Conexion.php";
+    $consulta = "Select foto,calle,numero_calle,telefono,fecha_nacimiento,fecha_ingreso,id_cp,id_colonia,id_access,activo,numero_interior,genero,CONCAT(apellido_p,' ',apellido_m,' ',nombre) as Nombre from cliente;";
+    $get = $db->query($consulta);
+    if($get){
+        $clientes=array();
+        $clientes2=array();
+        
+        while($res = mysqli_fetch_assoc($get)):
+            $clientes2[]=$res;
+        endwhile;
+        $clientes['clientes']=$clientes2;
+        echo json_encode($clientes);
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
 
 
+
+
+}
+function insertCliente($datos1,$cp1,$col1,$accesso1)
+{
+    include "Conexion.php";
+    //$f = "insert into cliente values(NULL,'".$datos1['nombre']."','".$datos1['apellidoP']."','".$datos1['apellidoM']."','".$datos1['foto']."','".$datos1['calle']."','".$datos1['numero']."','".$datos1['telefono']."',CURDATE(),".$cp1['id'].",".$col1['id'].",".$accesso1['id'].",'".$datos1['fechanac']."',1,'".$datos1['numeroint']."','".$datos1['gender']."');";
+    $f = "insert into cliente values(NULL,'".$datos1['nombre']."','".$datos1['apellidoP']."','".$datos1['apellidoM']."','".$datos1['foto']."','".$datos1['calle']."','".$datos1['numero']."','".$datos1['telefono']."',CURDATE(),".$cp1.",".$col1.",".$accesso1.",'".$datos1['fechanac']."',1,'".$datos1['numeroint']."','".$datos1['gender']."');";
+    //$f = "insert into cliente values(NULL,'edgar','sanchez','esp','cac','vfrv','15','33',CURDATE(),5,5,10,'03-05-21',1,'78','M');";
+    //echo json_encode($f);
+    $d = $db->query($f);
+    if($d){
+        echo json_encode("exito");
+    }
+    else{
+        echo json_encode("-1");
+    }
+    mysqli_close($db);
+}
+
+function addCliente($datos)
+{
+    $valAccesso = getAcceso($datos);    //verifica si el id es repetido
+    $accesso="0";
+    if($valAccesso=="0"){           //si no existe ese usuario
+
+        $accesso = setAcceso($datos);  //inserta los nuevos datos
+        $valCp=getCP($datos);  //verifica si ya existe el cp
+        $cp ="0";
+        if($valCp=="0"){  // si no existe lo inerta
+            $cp = setCP($datos);
+         }
+         else{      
+            $cp = $valCp['id'];  // si existe recupera la llave primaria
+        }
+
+        $valCol=getCol($datos);   //verifica si existe la colonia
+        $col = "0";
+        if($valCol=="0"){ //si no, la inserta
+            $col = setCol($datos);
+        }
+        else{
+            $col = $valCol['id'];  //si ya existe obtiene su llave primaria
+        }
+
+        if($cp!="0" & $col!="0" & $accesso!="0"){  /// si las llaves son distintas de cero
+            insertCliente($datos,$cp,$col,$accesso); 
+         }
+         else{
+
+             echo json_encode("-1"); 
+            } 
+    }
+    else{  /// si el id es repetido
+        echo json_encode("id_rep");
+    }  
+}
+function getAcceso($datos)
+{
+    include "Conexion.php";
+    $f = "select id from access where user='".$datos['user']."';";
+    $d = $db->query($f);
+    if($d){
+        if($res = mysqli_fetch_assoc($d)){
+            return $res;
+        }
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+    return "0";
+}
+function setAcceso($datos)
+{
+    include "Conexion.php";
+    $f = "insert into access values(NULL,'".$datos['user']."','".$datos['password']."','1');";
+    $d = $db->query($f);
+    if($d){
+        return mysqli_insert_id($db);
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+    
+}
+function getCP($datos)
+{
+    include "Conexion.php";
+    $f = "select id from cp where codigo='".$datos['cp']."';";
+    $d = $db->query($f);
+    if($d){
+        if($res = mysqli_fetch_assoc($d)){
+            return $res;
+        }
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+    return "0";
+}
+function setCP($datos)
+{
+    include "Conexion.php";
+    $f = "insert into cp values(NULL,'".$datos['cp']."');";
+    $d = $db->query($f);
+    if($d){
+        return mysqli_insert_id($db);
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+    
+}
+
+function getCol($datos)
+{
+    include "Conexion.php";
+    $f = "select id from colonia where nombre='".$datos['colonia']."';";
+    $d = $db->query($f);
+    if($d){
+        if($res = mysqli_fetch_assoc($d)){
+            return $res;
+        }
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+    return "0";
+    
+}
+function setCol($datos)
+{
+    include "Conexion.php";
+    $f = "insert into colonia values(NULL,'".$datos['colonia']."');";
+    $d = $db->query($f);
+    if($d){
+        return mysqli_insert_id($db);
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+   
+}
 
 function login($datos){
     include "Conexion.php";
@@ -41,6 +211,7 @@ function login($datos){
     else{
         echo json_encode(mysqli_error($db));
     }
+    mysqli_close($db);
     
 }
 
@@ -61,7 +232,31 @@ function mostrar($datos){
     else{
         echo json_encode(mysqli_error($db));
     }
+    mysqli_close($db);
 }
+
+function getLastId($datos){
+
+    include "Conexion.php";
+
+
+    $f = "select max(id_cliente) from cliente;";
+    $d = $db->query($f);
+
+    if($d){
+        if($res = mysqli_fetch_assoc($d)){
+            echo json_encode($res);
+        }
+        else{
+            echo json_encode("0");
+        }
+    }
+    else{
+        echo json_encode(mysqli_error($db));
+    }
+    mysqli_close($db);
+}
+
 
 
 ?>
